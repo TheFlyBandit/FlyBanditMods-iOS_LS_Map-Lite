0541af217de837d254baeb16586d2abc29596d8c
TheFlyBandit <102429379+TheFlyBandit@users.noreply.github.com>
2023-09-24 22:51:43 -0400

    Add files via upload

---
+++ FlyBanditMods-iOS_LS_Map-Lite/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'TheFlyBandit'
+description 'Custom iOS / Google Maps Themed Postal map for Los Santos'
+discord 'https://discord.gg/MH9BADQuZy'
+lua54 "yes"
+
+this_is_a_map 'yes'
+
+data_file "SCALEFORM_DLC_FILE" "stream/int3232302352.gfx"
+
+client_script "client.lua"
+
+
+files {
+  "stream/int3232302352.gfx"
+}
+
